cbb2c64f909168cf7f56c05712ee01a6a2ed2fc3
Tommie Gannert <tommie@gannert.se>
2024-02-04 20:46:49 +0100

    Adds a blink example.

---
+++ example/blink/blink.asm
@@ -0,0 +1,17 @@
+    dios
+
+    include "p16f887.inc"
+
+    module  "timer0.inc"
+    module  "blink.inc"
+    module  "init.inc"
+
+    const   option_init, and    ; The default is 0xFF.
+    const   intcon_init, or     ; The default is 0.
+    const   trisa_init, and     ; The default is 0xFF.
+    const   porta_init, or      ; The default is 0.
+
+    irq     irq_t0, INTCON, T0IF
+
+    evqueue IDLE_QUEUE, idle
+    event   BLINK_EVENT
